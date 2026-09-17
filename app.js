15ba1da00a6a8c3c4e84a4a218df726ebf8a9feb
NoodleSupreme <ethanc735@nycstudents.net>
2026-09-17 12:38:12 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+let calories = 800;
+
+if (calories <= 400) {
+        console.log("skin n bones")
+
+} else 
+    if (calories <= 2000 & calories >= 400) {
+        console.log("Average")
+    }
+
